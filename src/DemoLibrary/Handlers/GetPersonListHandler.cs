c67597d6bb83a830d19a1c391c2c82d39d60d08f
Chandan <Chandansah625@gmail.com>
2021-07-23 11:23:43 +0530

    Initial Commit

---
+++ src/DemoLibrary/Handlers/GetPersonListHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DemoLibrary.DataAccess;
+using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonListHandler : IRequestHandler<GetPersonListQuery, List<PersonModel>>
+    {
+        private readonly IDataAccess _data;
+        public GetPersonListHandler(IDataAccess data)
+        {
+            _data = data;
+        }
+        public Task<List<PersonModel>> Handle(GetPersonListQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
